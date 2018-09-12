3d788601ceeaa137b5e6744f71ce96c9f3ae53ba
Vladimir Halme <vhalme@gmail.com>
2018-09-13 01:11:29 +0300

    Updated video links

--- src/routes/invest/Invest.js
+++ src/routes/invest/Invest.js
@@ -191,7 +191,7 @@ class Invest extends React.Component {
               <iframe
                 width="100%"
                 height="400px"
-                src="https://www.youtube.com/embed/tgbNymZ7vqY"
+                src="https://www.youtube.com/watch?v=uGkCsshykF8"
               />
             </div>
           </div>
@@ -226,7 +226,7 @@ class Invest extends React.Component {
               <iframe
                 width="100%"
                 height="400px"
-                src="https://www.youtube.com/embed/tgbNymZ7vqY"
+                src="https://www.youtube.com/watch?v=YHDs51X2fGs"
               />
             </div>
           </div>
